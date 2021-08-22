03c04e0bfdd6eb61707af5d964c3cfed44e871f2
Wei <412723375@qq.com>
2021-08-22 15:46:34 +0800

    完整项目

--- service-gateway/src/main/java/com/myself/gateway/ServiceGatewayApplication.java
+++ service-gateway/src/main/java/com/myself/gateway/ServiceGatewayApplication.java
@@ -2,6 +2,7 @@ package com.myself.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 服务网关模块启动入口类
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 2021/7/3
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ServiceGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceGatewayApplication.class, args);
